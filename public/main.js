06e64d81de61519a2be4d4340dc1f8200b6aa1f4
Rhys Brame <rhysbrame@gmail.com>
2017-05-20 23:03:29 +0100

    managed to get API to work and requested club crests to web page

--- public/main.js
+++ public/main.js
@@ -1,5 +1,41 @@
 var app = function(){
-  console.log("Here we are...")
+  var url = 'http://api.football-data.org/v1/teams/86';
+  searchTeam();
+  makeRequest(url, requestComplete);
+}
+
+var searchTeam = function(){
+  
+  var searchQuery = document.getElementById('search-query');
+
+  searchQuery.addEventListener('keyup', function(){
+    var url = 'http://api.football-data.org/v1/teams/' + this.value;
+    makeRequest(url, requestComplete);
+  })
+
+}
+
+var makeRequest = function(url,callback){
+  var request = new XMLHttpRequest();
+  request.open("GET", url, true);
+  request.setRequestHeader('X-Auth-Token', '389231fe99ff411cb6e63d6a8aa3ea56');
+  request.addEventListener('load', requestComplete);
+  request.send();
+}
+
+var requestComplete = function(){
+  if(this.status !== 200) return;
+  var jsonString = this.responseText;
+  var teamData = JSON.parse(jsonString);
+  display(teamData);
+  console.log(teamData);
+}
+
+var display = function(teamData){
+  var htmlCrest = document.querySelector('img');
+  htmlCrest.src = teamData.crestUrl;
+  var htmlTeamName = document.getElementById('teamName');
+  htmlTeamName.innerText= teamData.name;
 }
 
 window.addEventListener('load', app);
